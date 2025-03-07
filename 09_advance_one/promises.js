bb5691543f242b8491d45be9c16861d110bd49d7
Sahil Kumar <145471343+sahil09kr@users.noreply.github.com>
2025-03-07 10:41:12 +0000

    promises studied

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,77 @@
+// const promise1=new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         console.log("asyc task done");
+//         resolve()
+        
+//     },5000)
+    
+// });
+
+// promise1.then(()=>{
+//     console.log("promise resolved");
+// })
+
+// new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("async task2 done")
+//         resolve()
+//     },1000)
+// }).then(()=>{
+//     console.log("again resolved");
+// })
+
+// const promise3= new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         console.log("ho gya time");
+//         resolve({username:"sahilkr09",email:"sahil@.com"})
+//     },1000)
+// })
+
+// promise3.then((data)=>{
+//     console.log(data);
+// })
+
+const promise4=new Promise ((resolve,reject)=>{
+    error=true;
+    if(!error){
+        setTimeout(()=>{
+            console.log("ho gya time");
+            resolve({username:"sahilkr09",email:"sahil@.com"})
+        },1000)
+    }
+    else{
+        reject("something went wrong");
+    }
+})
+
+// promise4.then((data)=>{
+//     console.log(data);
+//     return data.username;
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+// .finally(()=>{
+//     console.log("Promise4 is either rejected or resolved");
+// })
+
+async function consumePromise4(){
+    // const response=await promise4;
+    // console.log(response);
+    try{
+        const responce=await promise4;
+        console.log(responce);
+
+    } catch(error){
+        console.log(error);
+
+    } finally{
+        console.log("either tried and succesfull or OOPS!! error caught")
+    }
+//similar to .then()->.catch()->.fianlly();
+}
+
+consumePromise4();
