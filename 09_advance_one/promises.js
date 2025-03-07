a7f20786c1fb10ce3620c50f67efeca9c2cac267
Sahil Kumar <145471343+sahil09kr@users.noreply.github.com>
2025-03-07 11:59:49 +0000

    included async/await & .then(_

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -31,18 +31,18 @@
 //     console.log(data);
 // })
 
-const promise4=new Promise ((resolve,reject)=>{
-    error=true;
-    if(!error){
-        setTimeout(()=>{
-            console.log("ho gya time");
-            resolve({username:"sahilkr09",email:"sahil@.com"})
-        },1000)
-    }
-    else{
-        reject("something went wrong");
-    }
-})
+// const promise4=new Promise ((resolve,reject)=>{
+//     error=true;
+//     if(!error){
+//         setTimeout(()=>{
+//             console.log("ho gya time");
+//             resolve({username:"sahilkr09",email:"sahil@.com"})
+//         },1000)
+//     }
+//     else{
+//         reject("something went wrong");
+//     }
+// })
 
 // promise4.then((data)=>{
 //     console.log(data);
@@ -58,20 +58,66 @@ const promise4=new Promise ((resolve,reject)=>{
 //     console.log("Promise4 is either rejected or resolved");
 // })
 
-async function consumePromise4(){
-    // const response=await promise4;
-    // console.log(response);
-    try{
-        const responce=await promise4;
-        console.log(responce);
+// async function consumePromise4(){
+//     // const response=await promise4;
+//     // console.log(response);
+//     try{
+//         const responce=await promise4;
+//         console.log(responce);
 
-    } catch(error){
-        console.log(error);
+//     } catch(error){
+//         console.log(error);
 
-    } finally{
-        console.log("either tried and succesfull or OOPS!! error caught")
-    }
+//     } finally{
+//         console.log("either tried and succesfull or OOPS!! error caught")
+//     }
 //similar to .then()->.catch()->.fianlly();
+// }
+
+// consumePromise4();
+
+
+// fetch('https://api.github.com/users/hiteshchoudhary')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => console.log(error))
+
+
+
+// fetch('https://api.github.com/users/hiteshchoudhary')
+// .then((data)=>{
+//     return data.json();
+// })
+// .then((data)=>{
+//     return data.name;
+// })
+// .then((data)=>{
+// console.log(data)
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+async function getData(){
+
+    try{
+    const gotit=await(fetch('https://api.github.com/users/hiteshchoudhary'))
+    // console.log(gotit);
+    const converted= await gotit.json();
+    console.log(converted);
+    }catch(err){
+        console.log(err);
+    }finally{
+        console.log("kuch to hua hai resolve ya reject")
+    }
+
+
 }
 
-consumePromise4();
+getData();
+
+
